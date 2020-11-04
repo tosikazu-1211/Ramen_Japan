7d46ff72a0163c60ffdeeddf74add7035d86eaeb
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-04 04:02:05 +0000

    [Add] admin レビューの削除アクションの追加

--- app/controllers/admin/ramen_noodles_controller.rb
+++ app/controllers/admin/ramen_noodles_controller.rb
@@ -1,10 +1,20 @@
 class Admin::RamenNoodlesController < ApplicationController
-	#レビュー一覧ページ
-	def index
-		@ramen_noodles = RamenNoodle.page(params[:page]).reverse_order
-	end
+  before_action :authenticate_admin!
 
-	def show
-		@ramen_noodle = RamenNoodle.find(params[:id])
-	end
+  # レビュー一覧ページ
+  def index
+    @ramen_noodles = RamenNoodle.page(params[:page]).reverse_order
+  end
+
+  # レビュー詳細ページ
+  def show
+    @ramen_noodle = RamenNoodle.find(params[:id])
+  end
+
+  # レビュー削除アクション
+  def destroy
+    @ramen_noodle = RamenNoodle.find(params[:id])
+    @ramen_noodle.destroy
+    redirect_to admin_ramen_noodles_path
+  end
 end
